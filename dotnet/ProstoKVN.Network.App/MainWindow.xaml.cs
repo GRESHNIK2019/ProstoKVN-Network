0466d541afea5751b03d7b35e9bbac5c2588d3b8
Draires <45656626+GRESHNIK2019@users.noreply.github.com>
2026-08-19 17:27:12 +0300

    Исправить namespace MicaBackdrop для WinUI 3

--- dotnet/ProstoKVN.Network.App/MainWindow.xaml.cs
+++ dotnet/ProstoKVN.Network.App/MainWindow.xaml.cs
@@ -1,6 +1,6 @@
-using Microsoft.UI.Composition.SystemBackdrops;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
 using ProstoKVN.Network.App.ViewModels;
 
 namespace ProstoKVN.Network.App;
